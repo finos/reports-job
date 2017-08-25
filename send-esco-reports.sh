4d88e689c26e369311b7f6675bc3440877eb2768
Peter Monks <pmonks@gmail.com>
2017-08-25 10:43:14 -0700

    :tada: Initial commit of logic

---
+++ send-esco-reports.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Only run the build if it was triggered by Travis CI's cron facility
+if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
+    echo "This build was not triggered by cron - skipping."
+    exit 0
+fi
+
+# Check that we have all the necessary env vars (configured in .travis.yml)
+declare -a vars=(GITHUB_USER GITHUB_TOKEN JIRA_USER JIRA_PASSWORD BITERGIA_USER BITERGIA_PASSWORD SMTP_USER SMTP_PASSWORD ESCO_EMAIL_ADDRESS)
+
+for var_name in "${vars[@]}"
+do
+  if [ -z "$(eval "echo \$$var_name")" ]; then
+    echo "Missing environment variable $var_name"
+    exit 1
+  fi
+done
+
+# Ok we're good to go!
+mkdir target
+cd target
+git clone https://$GITHUB_TOKEN@github.com/symphonyoss/ssf-metadata.git
+cd ssf-metadata/tools/metadata-tool
+lein deps
+lein run -- email-active-projects-with-unactioned-prs-report email-inactive-projects-report

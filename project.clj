f5f5c84a17f06ddd1f3125f2ca8a7f10e3794129
Peter Monks <pmonks@gmail.com>
2017-08-25 11:09:16 -0700

    :ambulance: Add placeholder project.clj to prevent TravisCI from barfing

---
+++ project.clj
@@ -0,0 +1,27 @@
+;
+; Copyright © 2017 Symphony Software Foundation
+; SPDX-License-Identifier: Apache-2.0
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+;
+
+(defproject org.symphonyoss.symphony/ssf-job-esco-reports "0.1.0-SNAPSHOT"
+  :description      "This placeholder project file is here only to prevent TravisCI from barfing."
+  :url              "https://github.com/symphonyoss/bot-unfurl"
+  :license          {:spdx-license-identifier "Apache-2.0"
+                     :name                    "Apache License, Version 2.0"
+                     :url                     "http://www.apache.org/licenses/LICENSE-2.0"}
+  :min-lein-version "2.7.0"
+  :dependencies     [
+                      [org.clojure/clojure "1.8.0"]
+                    ])
